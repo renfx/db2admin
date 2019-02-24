3fb6f781659b733db5db81bd33f90af66bbfaec0
rfx <77830749@qq.com>
2019-02-24 14:07:32 +0800

    查詢條件 toolbar

--- src/main/java/com/rfxdevelop/db2admin/controller/AdminController.java
+++ src/main/java/com/rfxdevelop/db2admin/controller/AdminController.java
@@ -1,14 +1,11 @@
 package com.rfxdevelop.db2admin.controller;
 
-import com.alibaba.druid.support.json.JSONUtils;
-import com.alibaba.fastjson.JSONObject;
 import com.rfxdevelop.db2admin.model.BaseData;
 import com.rfxdevelop.db2admin.model.DbTable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,7 +22,6 @@ public class AdminController {
     public ModelAndView view(@PathVariable("tableName") String tableName,ModelAndView modelAndView) throws Exception {
         modelAndView.setViewName("right");
         DbTable dbTable = baseData.getTableMap().get(tableName);
-        String jsonString = JSONObject.toJSONString(dbTable);
         modelAndView.addObject("tableName",tableName);
         modelAndView.addObject("tableData", dbTable);
         return modelAndView;
--- src/main/java/com/rfxdevelop/db2admin/controller/BasicController.java
+++ src/main/java/com/rfxdevelop/db2admin/controller/BasicController.java
@@ -1,6 +1,7 @@
 package com.rfxdevelop.db2admin.controller;
 
 import com.alibaba.druid.support.json.JSONUtils;
+import com.alibaba.fastjson.JSONObject;
 import com.rfxdevelop.db2admin.dao.dynamic.IMapper;
 import com.rfxdevelop.db2admin.dao.dynamic.exception.MapperParamException;
 import com.rfxdevelop.db2admin.model.Result;
@@ -8,6 +9,7 @@ import com.rfxdevelop.db2admin.utils.RequestParamUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +34,7 @@ public class BasicController {
     public Result entrance(HttpServletRequest request, @PathVariable("method") String methodStr, @RequestParam String tableName) throws Exception {
         Result result = Result.successResult();
         Map<String, String> params = RequestParamUtil.getParams(request);
+        toPageQuery(params);
         String[] methods = methodStr.split("-");
         for(String method:methods){
             Method mapperMethod = Stream.of(IMapper.class.getDeclaredMethods())
@@ -43,6 +46,26 @@ public class BasicController {
             result.put(method,invoke);
         }
         return result;
+    }
+
+    private void toPageQuery(Map<String, String> params){
+        String pageQuery = params.get("pageQuery");
+        String page = params.get("page");
+        String limit = params.get("limit");
+        String orderBy = params.get("orderBy");
+        String orderType = params.get("orderType");
+        JSONObject  pageQueryMap = new JSONObject();
+        if(StringUtils.hasText(page) && StringUtils.hasText(limit)){
+            pageQueryMap.put("pageNo",Integer.parseInt(page));
+            pageQueryMap.put("pageSize",Integer.parseInt(limit));
+        }
+        if(StringUtils.hasText(orderBy) &&StringUtils.hasText(orderType)){
+            pageQueryMap.put("orderBy",orderBy);
+            pageQueryMap.put("orderType",orderType);
+        }
+        if(!StringUtils.hasText(pageQuery)){
+            params.put("pageQuery",pageQueryMap.toJSONString());
+        }
     }
 
 
